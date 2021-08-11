6a74030566c5ee39a52ab618bc7fef0bc23e562d
Andrey Yanduganov <andreybotanic@gmail.com>
2021-08-11 07:57:13 +0700

    Remove unused imports

--- src/main/java/com/andreybotanic/asm/nasm/psi/NasmTokenType.java
+++ src/main/java/com/andreybotanic/asm/nasm/psi/NasmTokenType.java
@@ -1,11 +1,9 @@
 package com.andreybotanic.asm.nasm.psi;
 
 import com.andreybotanic.asm.nasm.NasmLanguage;
-import com.intellij.lang.Language;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class NasmTokenType extends IElementType {
     public NasmTokenType(@NonNls @NotNull String debugName) {
